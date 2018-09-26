e951de57e956f9c6ba64366505aa664da9bad165
Jeromevig <jer.vignerot@gmail.com>
2018-09-26 09:15:39 +0200

    Initial Commit.

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+    use HelloWorld\SayHello;
+
+    echo SayHello::world();
+
+    use App\Wcs\Hello;
+
+    echo Hello:: talk();
